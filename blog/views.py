8b4092061f5bebcd1d9a542237a4af23af6f30a2
Your Name <you@example.com>
2024-03-24 16:53:37 +0530

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,43 @@
-from django.shortcuts import render, get_object_or_404 ,redirect
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.http import HttpResponse
+from django.template.loader import get_template
+from django.shortcuts import get_object_or_404
+from xhtml2pdf import pisa
+from .models import Post
+
+def generate_pdf(request, pk):
+    # Get the post object
+    post = get_object_or_404(Post, pk=pk)
+
+    # Load the template
+    template_path = 'blog/post_detail.html'
+    context = {'post': post}
+    template = get_template(template_path)
+    html = template.render(context)
+
+    # Remove unwanted elements from the HTML content
+    html = html.replace('Django Girls Blog', '')  # Remove "Django Girls Blog"
+    html = html.replace('<a href="{% url \'generate_pdf\' pk=post.pk %}" target="_blank">Download PDF</a>', '')  # Remove "Download PDF" link
+
+    # Create a PDF response
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = f'attachment; filename="{post.title}.pdf"'
+
+    # Generate PDF
+    pisa_status = pisa.CreatePDF(html, dest=response)
+    if pisa_status.err:
+        return HttpResponse('PDF creation failed', status=500)
+
+    return response
+
+
 
 
 def post_list(request):
@@ -12,18 +48,16 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
-
-def post_new(request):
-    form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
-
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
+            if request.user.is_authenticated:
+                post.author = request.user
+            else:
+                # Fallback to a default user if the request user is not authenticated
+                post.author = User.objects.first()  # Change this to your desired fallback user
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -37,10 +71,22 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-        form = PostForm(request.POST, instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
